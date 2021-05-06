7792e0ad8802b016908bfb4abc7085ba92f17885
dreadheaddreamz <github email address>
2021-05-05 23:51:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching (source, drivers) {
+return source.filter( possibleMatch => 
+    possibleMatch.toLowerCase() === drivers.toLowerCase()
+    )
+}
+
+function fuzzyMatch(source, test) {
+    return source.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(test.toLowerCase()) === 0 
+    )
+}
+
+function matchName(source, sought) {
+    return source.filter( record => record.name === sought)
+}
